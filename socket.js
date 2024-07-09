1b4941b17d973e4fd5b523c90ec4682bcfef3802
erdison <109794675+Soni912108@users.noreply.github.com>
2024-07-09 12:33:04 +0200

    Update socket.js

--- socket.js
+++ socket.js
@@ -2,9 +2,11 @@
 const http = require('http');
 const express = require('express');
 const socketIo = require('socket.io');
+const path = require('path');
 
 const app = express();
 const server = http.createServer(app);
+//adding path to socket.io
 const io = socketIo(server, {
   path: '/socket.io'
 });
