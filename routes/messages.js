0427176a01458f1afbe07c82871899cde128284b
erdison <109794675+Soni912108@users.noreply.github.com>
2024-07-13 13:13:32 +0200

    Update messages.js

--- routes/messages.js
+++ routes/messages.js
@@ -1,13 +1,11 @@
-//routes/messages.js
-
 const express = require('express');
 const Message = require('../models/Messages');
 const router = express.Router();
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
-const Notification = require('../models/Notification');
 
 module.exports = (app, redisClient) => {
+
   // Function to fetch messages from Redis (if available)
   async function getMessagesFromRedis(roomId) {
     const key = `room:${roomId}:messages`;
@@ -24,9 +22,13 @@ module.exports = (app, redisClient) => {
         messagesWithUsername.push({
           username: user ? user.username : null, // Handle potential missing user
           content: message.content,
+          timestamp: message.timestamp // Include the timestamp
         });
       }
 
+      // Sort messages by timestamp in descending order
+      messagesWithUsername.sort((a, b) => new Date(a.timestamp) - new Date(b.timestamp));
+      
       return messagesWithUsername;
     } catch (error) {
       console.error('Error fetching messages from Redis:', error);
@@ -36,25 +38,29 @@ module.exports = (app, redisClient) => {
 
   router.get('/:roomId', async (req, res) => {
     const { roomId } = req.params;
-  
+
     try {
       // Try fetching from Redis first
       let messagesWithUsername = await getMessagesFromRedis(roomId);
-  
+
       if (messagesWithUsername) {
         res.json({ messageTuples: messagesWithUsername });
       } else {
         // If not found in Redis, fetch from MongoDB
-        let messages = await Message.find({ room: roomId }).populate('user', 'username'); // Populate only 'username' field
-  
+        let messages = await Message.find({ room: roomId })
+          .populate('user', 'username') // Populate only 'username' field
+          .sort({ createdAt: 1 }); // Sort by creation date in descending order
+
         if (!messages.length) {
           return res.status(404).json({ message: 'No messages found for this room' });
         }
-  
+
         const messageTuples = messages.map(message => ({
           username: message.user.username,
-          content: message.content
+          content: message.content,
+          timestamp: message.createdAt // Include the timestamp
         }));
+        
         res.json({ messageTuples });
       }
     } catch (error) {
@@ -62,9 +68,6 @@ module.exports = (app, redisClient) => {
       res.status(500).json({ message: 'Internal server error' });
     }
   });
-  
 
   app.use('/api/messages', router); // Mount routes
 };
-
-
