b86a1493a29e550c21dc8ef8f2f40ae09aa3787f
erdison <109794675+Soni912108@users.noreply.github.com>
2024-10-19 11:43:37 +0200

    Update mongodbConnection.js

--- databases/mongodbConnection.js
+++ databases/mongodbConnection.js
@@ -1,18 +1,23 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+const connectToMongoDB = async () => {
+  const password = process.env.PASSWORD; // Access password from environment variables
+  const encodedPassword = encodeURIComponent(password); // Encode the password to handle special characters
+  const username = process.env.USER; // Access username from environment variables
+  const MONGODB_URI = process.env.MONGODB_URI // Access the full URI with placeholders to replace
 
-const connectToMongoDB = () => {
-  const password = process.env.PASSWORD;
-  const encodedPassword = encodeURIComponent(password);
+    // Replace the placeholders with actual values
+    .replace('USERNAME_PLACEHOLDER', username)
+    .replace('PASSWORD_PLACEHOLDER', encodedPassword);
 
-  mongoose.connect(
-    `mongodb+srv://sonimailfortestuse:${encodedPassword}@backenddb.yf5ipol.mongodb.net/chat-app?retryWrites=true&w=majority&appName=BackendDB`
-  ).then(() => {
-    console.info("Connected to MongoDB");
-  }).catch((error) => {
-    console.error("Connection failed!", error);
-  });
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.info('Connected to MongoDB');
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
 };
 
 module.exports = connectToMongoDB;
+
